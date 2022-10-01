c43aa967cbb685d38a4fbc1ec13893588a75f035
DESKTOP-VSQTI1I\Dell <sw.dev.bsaltos@gmail.com>
2022-09-30 20:35:01 -0500

    configuracion inicial de dependencias en node con  npm install @babel/core babel-loader html-webpack-plugin webpack webpack-cli webpack-dev-server --same-dev. configuracion de webpack

---
+++ cientifico/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { resolve } = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    }, 
+    plugins: [
+        new HtmlWebpackPlugin([
+            {
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html',
+            }
+        ]),
+    ]
+}
