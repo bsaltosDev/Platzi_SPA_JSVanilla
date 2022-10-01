f2ae0fec3205dc19f452a5e383b5f9c4080122c2
DESKTOP-VSQTI1I\Dell <sw.dev.bsaltos@gmail.com>
2022-09-30 22:39:11 -0500

    render de template de Home usando el fetch de la data y los templates literals usando map para recorrer la data y crear el template respectivo del presonaje, se usa join para eliminar caracters no deseado al unir la data del array

--- cientifico/src/pages/Home.js
+++ cientifico/src/pages/Home.js
@@ -1,15 +1,20 @@
-const Home = () => {
-    const view = `
-      <div class="Characters">
+import getData from "../utils/getData";
+
+const Home = async() => {
+  const characters = await getData();
+  const view = `
+    <div class="Characters">
+      ${characters.results.map(character => `
         <article class="Character-item">
-          <a href="#/1/">
-            <img src="image" alt="name">
-            <h2>Name</h2>
-          </a>
-        </article>
-      </div>
-    `;
-    return view;
-  };
+        <a href="#/${character.id}/">
+          <img src="${character.image}" alt="${character.name}">
+          <h2>${character.name}</h2>
+        </a>
+      </article>
+      `).join('')}
+    </div>
+  `;
+  return view;
+};
   
-  export default Home;
+export default Home;
