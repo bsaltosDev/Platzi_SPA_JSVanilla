0377e94e1e8690e11428933dfce1c868ccd40d8d
DESKTOP-VSQTI1I\Dell <sw.dev.bsaltos@gmail.com>
2022-09-30 22:23:43 -0500

    resolver rutas de hash #

--- cientifico/src/index.js
+++ cientifico/src/index.js
@@ -1,2 +1,3 @@
 import router from './routes';
 window.addEventListener('load', router); //SPA, cuando esten todos los archivos cargados cargamos el router
+window.addEventListener('hashchange', router); //listener para cambio de rutas
